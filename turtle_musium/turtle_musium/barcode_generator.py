132019083c423e366bc610500ddf53ed69d16ece
minjae <999aldwo@gmail.com>
2025-08-30 14:19:48 +0900

    바코드 생성, 인식 업로드

---
+++ turtle_musium/turtle_musium/barcode_generator.py
@@ -0,0 +1,67 @@
+import os
+import barcode
+from barcode.writer import ImageWriter
+from datetime import datetime
+
+# 날짜
+# 인원수
+# 기념품
+
+class BarcodeGenerator:
+    def __init__(self, count_people="3", gift_data="1001", directory=None):
+        """
+        BarcodeGenerator 클래스 초기화
+        :param count_people: 인원수 (기본값: 3)
+        :param gift_data: 기념품 (기본값: 1001)
+        :param directory: 바코드를 저장할 디렉토리 (기본값: 현재 파일 기준)
+        """
+        self.count_people = count_people
+        self.gift_data = gift_data
+        self.directory = directory or os.path.dirname(os.path.abspath(__file__))  # 디렉토리 기본값 설정
+
+    def create_bar_code(self):
+        """ 날짜, 시간, 무료 금액 정보를 포함한 바코드 생성 """
+        date_data = datetime.now().strftime("%Y%m%d")
+        bar_data = f"{date_data}-{self.count_people}-{self.gift_data}"
+
+        # 바코드 생성 (Code128 바코드 형식 사용)
+        code128 = barcode.get_barcode_class('code128')
+        barcode_instance = code128(bar_data, writer=ImageWriter())
+
+        # 저장할 경로 설정
+        save_path = os.path.join(self.directory, "bar_codes")
+        os.makedirs(save_path, exist_ok=True)  # 폴더가 없으면 생성
+        file_path = os.path.join(save_path, f"bar_code_{bar_data}")  # 파일 경로 설정
+
+        options = {
+            'module_width': 0.5,     # 선 굵기 키우기
+            'module_height': 30.0,   # 세로 높이 늘리기
+            'quiet_zone': 6.5,       # 여백 확보
+            'write_text': True       # 아래 텍스트 제거
+        }
+        barcode_instance.save(file_path, options)  # 바코드 저장
+
+        # barcode_instance.save(file_path)  # 바코드 저장
+
+        print(f"바코드 생성 완료: {file_path}")
+        return file_path
+
+# 외부에서 호출 예시
+DUMMY = [
+    {
+        "count_people": "3",
+        "gift_data": "1001",
+    },
+    {
+        "count_people": "2",
+        "gift_data": "0110",
+    }
+]
+if __name__ == "__main__":
+    dummy_idx = 1
+    barcode_generator = BarcodeGenerator(
+        count_people=DUMMY[dummy_idx]["count_people"], 
+        gift_data=DUMMY[dummy_idx]["gift_data"],
+        directory="/home/rokey/turtlebot4_ws/src/turtle_musium/turtle_musium"
+    )  # 원하는 금액으로 초기화
+    barcode_generator.create_bar_code()  # 바코드 생성
---
+++ turtle_musium/turtle_musium/barcode_scanner.py
@@ -0,0 +1,102 @@
+import cv2
+import threading
+from pyzbar.pyzbar import decode
+# import winsound
+import os
+from PyQt5.QtCore import pyqtSignal, QObject
+from datetime import datetime
+
+'''
+Thread 사용 이유
+간단한 프로젝트에서는 QTimer가 UI 이벤트 루프에서 동작하여 간편하고 유용하지만,
+프로젝트의 확장성을 우려하고, 카메라와 UI를 독립적으로 나누기 위해 Thread 선택
+또한, 명시적인 스레드 종료 및 자원 해제를 통해 라이프 사이클을 완전히 컨트롤 할 수 있다.
+'''
+
+
+class BarcodeScannerWorker(QObject):
+    frameCaptured = pyqtSignal(object)  # 프레임 업데이트 신호
+    barcodeDetected = pyqtSignal(str)   # 바코드 정보 신호
+    errorOccurred = pyqtSignal(str)     # 에러 발생 시그널 추가
+
+    def __init__(self):
+        super().__init__()
+        self.running = False
+        self.cap = cv2.VideoCapture(0)
+        self.thread = None  # 스레드 객체
+
+    def start(self):
+        if self.thread is None or not self.thread.is_alive():  # 기존 스레드가 없거나 종료되었을 때만 실행
+            self.running = True
+            self.thread = threading.Thread(target=self.run)
+            self.thread.start()
+
+    def run(self):
+        while self.running:
+            ret, frame = self.cap.read()
+            if not ret:
+                self.errorOccurred.emit("카메라를 열 수 없습니다.")
+                break
+
+            frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+            
+            processed_frame, barcode_info, detected = self.recognize_barcode(frame)
+            self.frameCaptured.emit(processed_frame)
+            
+            if detected:
+                self.barcodeDetected.emit(barcode_info)
+                self.running = False
+                
+        self.cap.release()
+    
+    def recognize_barcode(self, frame):
+        barcodes = decode(frame)
+        barcode_data = None
+        for barcode in barcodes:
+            if barcode.type == 'PDF417':
+                continue
+            x, y, w, h = barcode.rect
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            barcode_data = barcode.data.decode('utf-8')
+            barcode_type = barcode.type
+            text = f'{barcode_type}: {barcode_data}'
+            cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.9, (0, 255, 0), 2)
+            # winsound.Beep(1000, 200)
+            os.system('beep -f 1000 -l 200')
+            return frame, barcode_data, True
+        return frame, barcode_data, False
+    
+    def stop(self):
+        self.running = False
+        if self.thread:
+            self.thread.join()
+
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
+    worker = BarcodeScannerWorker()
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("카메라를 열 수 없습니다.")
+            break
+
+        frame, barcode_data, detected = worker.recognize_barcode(frame)
+        cv2.imshow("Barcode Scanner", frame)
+
+        if detected:
+            obj_nowdate = datetime.strptime(barcode_data.split('-')[0], "%Y%m%d")
+            count_people = barcode_data.split('-')[1]
+            gift_data = barcode_data.split('-')[2]
+            # info = {'datetime':obj_nowdate, 'price':free_amount}
+            print("바코드 감지됨:")
+            print(f"날짜: {obj_nowdate}, 인원수: {count_people}, 기념품: {gift_data}")
+            break
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
